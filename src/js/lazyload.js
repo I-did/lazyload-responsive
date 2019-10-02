6e78bf5852fd121684f15984078e63236a2d3d18
I-did <89224714290a@gmail.com>
2019-10-02 22:47:55 +0300

    upload images, create basic structure

--- src/js/lazyload.js
+++ src/js/lazyload.js
@@ -1,3 +1,17 @@
-;(function() {
-	
-})();
+;(function(factory) {
+	if (typeof define === "function" && define.amd) {
+		define([], factory);
+	} else if (typeof exports === "object") {
+		module.exports = factory();
+	} else {
+		window.lazyload = factory();
+	}
+})(function() {
+	class lazyload {
+		constructor(options) {
+			this.options = options;
+			console.log(this);
+		}
+	}
+	return lazyload;
+});
