b2dd14dfc0dd1ae805b77c32af9f4ac3da8384c5
I-did <89224714290a@gmail.com>
2019-10-04 12:10:43 +0300

    create class options

--- src/js/lazyload.js
+++ src/js/lazyload.js
@@ -7,10 +7,35 @@
 		window.lazyload = factory();
 	}
 })(function() {
+	let _,
+		opt = {},
+		id = 0;
+
 	class lazyload {
 		constructor(options) {
-			this.options = options;
-			console.log(this);
+			_ = this;
+			_.defaults = {
+				mode: 'progressive',
+				selector: '.lazy',
+				srcAttr: 'data-src',
+				mediaAttr: 'data-media'
+			};
+
+			if (selectors) {
+				if (typeof selectors === 'string') {
+					opt = _.options = options;
+					opt.selector = selectors;
+				} else {
+					opt = _.options = selectors;
+				}
+			}
+
+			for (let name in _.defaults) {
+				if (opt[name] === undefined) {
+					opt[name] = _.defaults[name];
+				}
+			}
+
 		}
 	}
 	return lazyload;
